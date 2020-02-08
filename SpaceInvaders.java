9d85d62b979fa31f1de84db9cd178f437b3e5c40
Francesco-Falsetti-Peano-4A <56470899+Francesco-Falsetti-Peano-4A@users.noreply.github.com>
2020-02-08 10:14:40 +0100

    Aggiunti file java progetto SpaceInvaders STEP 1

---
+++ SpaceInvaders.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+/**
+ *
+ * @author longhitano.stefano
+ */
+public class SpaceInvaders extends JFrame {
+
+    public SpaceInvaders() {
+        this.setVisible(true);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Intro intro = new Intro();
+        this.add(intro);
+        // impacchetto il tutto e rendi visibile
+        this.pack();
+        this.setVisible(true);
+        // avvia l'introduzione
+        intro.run();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
